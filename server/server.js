6ab7706f12d95a1f0a05d1b3fe5a4a08f8686afa
JayesNS <jakubszkarlat@gmail.com>
2018-08-22 15:09:31 +0200

    Basic Socket.io setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const app = express()
+const http = require('http').Server(app)
+const sio = require('socket.io')()
 
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.static('client'))
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Running server at ${port}`)
+})
+sio.listen(http)
+
+let connections = []
+sio.on('connection', (socket) => {
+  connections.push(socket)
+  console.log('User connected to server')
+
+  socket.emit('connection info', {
+    users: connections.length
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected from server')
+    connections.splice(connections.indexOf(socket), 1)
+  })
 })
